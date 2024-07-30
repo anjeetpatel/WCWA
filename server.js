9c4cb380146b39ac75e869d402063159d344e5ac
Yug2099 <77870143+Yug2099@users.noreply.github.com>
2024-07-30 19:34:22 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-const PORT = process.env.PORT || 5000;
-
-app.use(express.json());
-
-const server = app.listen(PORT, console.log(`Server started at port ${PORT}`));
